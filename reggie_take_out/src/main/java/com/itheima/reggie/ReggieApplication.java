a8851797c63cb7349b726649a64de51b1fe3df89
sw <1927234700@qq.com>
2024-08-05 20:25:39 +0800

    wu

--- reggie_take_out/src/main/java/com/itheima/reggie/ReggieApplication.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
+//sunwu xuigai
 @Slf4j
 @SpringBootApplication
 @ServletComponentScan
